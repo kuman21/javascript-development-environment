3180d71bc0f877fc04529a1458a57e6ef6019902
Victor Tovar <victog21@outlook.com>
2020-02-22 12:33:59 -0600

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    optimization: {
+        minimizer: [ new OptimizeCssAssetsPlugin() ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+    ]
+
+}
